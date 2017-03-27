46a4c754528ce5fe28d868448fd61867539e683d
Your Name <you@example.com>
2017-03-27 19:47:01 +0400

    conf

--- app/addons/csc_bsg_world/config.php
+++ app/addons/csc_bsg_world/config.php
@@ -1,5 +1,5 @@
 <?php
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
-fn_define('BSG_VERSION_CHECK_URL', 'http://127.0.0.1/version.html');
-fn_define('BSG_FEEDBACK_MAIL', 'skater4@yandex.ru');
+fn_define('BSG_VERSION_CHECK_URL', 'http://dev.cs-coding.com/bsg/version.html');
+fn_define('BSG_FEEDBACK_MAIL', 'support@bsg.world');
