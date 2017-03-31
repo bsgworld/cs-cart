8f8e85efc736e7142427f0df00fb4fe68257186a
Your Name <you@example.com>
2017-03-31 21:58:47 +0400

    rdy

--- app/addons/csc_bsg_world/config.php
+++ app/addons/csc_bsg_world/config.php
@@ -1,5 +1,5 @@
 <?php
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
-fn_define('BSG_VERSION_CHECK_URL', 'http://dev.cs-coding.com/bsg/version.html');
+fn_define('BSG_VERSION_CHECK_URL', 'https://bsg.world/cs-cart.json');
 fn_define('BSG_FEEDBACK_MAIL', 'support@bsg.world');
